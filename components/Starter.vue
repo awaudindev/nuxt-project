0ed8ba5776cd6bedb3387a20570976af7f6668bc
awaludin <awaludin.dev@gmail.com>
2022-11-16 17:02:31 +0700

    first commit

---
+++ components/Starter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gank-gray sm:items-center sm:pt-0">
+    <header class="w-full bg-gank-gray">
+        <div class="w-full flex justify-between items-center 2xl:mx-auto 2xl:w-[1500px]">
+            <div class="block">
+                <nuxt-logo/>
+            </div>
+        </div>
+    </header>
+  </div>
+</template>
+
+<script>
+import NuxtLogo from './NuxtLogo.vue'
+export default {
+  name: 'Starter'
+}
+</sc
+    NuxtLogoript>
